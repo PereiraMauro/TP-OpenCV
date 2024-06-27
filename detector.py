1bc8f340de675df73e3f41f958af670b2d0ee296
PereiraMauro <pereiramauroezequiel@gmail.com>
2024-06-27 16:13:38 -0300

    Update detector.py
    se ajustaron parametros y se puso una funcion para que solo suene una alerta a la vez

--- detector.py
+++ detector.py
@@ -12,22 +12,27 @@ class DriverFatigueDetector:
         self.drowsy_threshold = 5  # seconds
         self.drowsy_start_time = None
         pygame.mixer.init()  # Initialize the mixer for pygame
+        self.sound_playing = False
 
     def detect_fatigue(self, frame):
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = self.face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5)
+        faces = self.face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
         drowsy_detected = False
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
             face_roi = gray_frame[y:y+h, x:x+w]
-            eyes = self.eye_cascade.detectMultiScale(face_roi)
+            eyes = self.eye_cascade.detectMultiScale(face_roi, scaleFactor=1.1, minNeighbors=3, minSize=(20, 20))
             if len(eyes) == 0:
                 if self.drowsy_start_time is None:
                     self.drowsy_start_time = time.time()
                 elif time.time() - self.drowsy_start_time >= self.drowsy_threshold:
                     drowsy_detected = True
-                    alert_sound = pygame.mixer.Sound('assents/Alerta.mp3')
-                    alert_sound.play()
+                    if not self.sound_playing:
+                        alert_sound = pygame.mixer.Sound('assents/Alerta.mp3')
+                        alert_sound.play()
+                        self.sound_playing = True
             else:
                 self.drowsy_start_time = None
+                self.sound_playing = False
         return frame, drowsy_detected
+    
