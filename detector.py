4cbda9fdae1f1f95bb9864f049be963ba139af72
PereiraMauro <pereiramauroezequiel@gmail.com>
2024-06-27 22:05:29 -0300

    Update de detector.py lbl
    se agrego un modelo de mascara para poder poder hacer un reconocimiento facial mas optimo

--- detector.py
+++ detector.py
@@ -4,12 +4,13 @@ import cv2
 import time
 import pygame
 import os
+import numpy as np
 
 class DriverFatigueDetector:
     def __init__(self):
         self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
         self.landmark_detector = cv2.face.createFacemarkLBF()
-        self.landmark_detector.loadModel('lbfmodel.yaml')
+        self.landmark_detector.loadModel('assents/lbfmodel.yaml')
         self.drowsy_threshold = 5  # seconds
         self.drowsy_start_time = None
         pygame.mixer.init()  # Initialize the mixer for pygame
@@ -22,10 +23,12 @@ class DriverFatigueDetector:
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
             face_roi = gray_frame[y:y+h, x:x+w]
-            _, landmarks = self.landmark_detector.fit(face_roi, faces)
+            _, landmarks = self.landmark_detector.fit(gray_frame, np.array([[(x, y, x+w, y+h)]]))
             for landmark in landmarks:
-                for (x, y) in landmark[0][36:48]:  # Eye landmarks are points 36-47
-                    cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
+                for (lx, ly) in landmark[0][36:48]:  # Eye landmarks are points 36-47
+                    lx = int(lx)
+                    ly = int(ly)
+                    cv2.circle(frame, (lx, ly), 2, (0, 255, 0), -1)
             if len(landmarks) == 0:
                 if self.drowsy_start_time is None:
                     self.drowsy_start_time = time.time()
@@ -39,4 +42,5 @@ class DriverFatigueDetector:
                 self.drowsy_start_time = None
                 self.sound_playing = False
         return frame, drowsy_detected
+
     
